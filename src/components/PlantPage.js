29562328b83db2ba147e42b779039e9d083ce798
Andrew Fuller <fuller.andrew55@gmail.com>
2024-10-03 00:33:28 -0500

    Fully functional plant app

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plant) => setPlants(plant));
+  }, []);
+
+  //Plant form spread hanlder
+  function onAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  //Create var used to render plants based on search and JSON
+  const plantsToDisplay = plants
+    .filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant}/>
+      <Search onSearchChange={setSearch} search={search}/>
+      <PlantList plants={plantsToDisplay}/>
     </main>
   );
 }
